7552bd9a826300a3c4c429c8e0262f3b5e5eaa2c
Trevor Hoglund <trewbot@phene.co>
2017-09-29 21:07:50 -0500

    refactor

--- js/2/assets/physics.js
+++ js/2/assets/physics.js
@@ -8,6 +8,93 @@
  *  2017.09.29
  */
 
+class Phys {
+    /**
+     *	Physics functions
+     *  @param {number} [t=0.50e3] Amount of seconds in one tick
+     *  @param {function} callback Callback function
+     */
+    constructor(t=0.50e3,callback=()=>{}){
+        //  Declaration of physical constants
+        this.G      = 6.67e-11; // N m^2 kg^-2
+        this.ℏ		= 1.05e-34; // m^2 kg / s
+        this.t      = t;        // s
+        this.particles = [];
+        this.steps = [
+            this.gravity,
+            this.move
+        ];
+        this.running= true;
+        callback();
+    }
+
+    /**
+     *  Find the center of mass of an array of particles
+     *  @param {array} particles Array of particles to analyze
+     */
+    static centerOfMass(particles){
+        var sum     = 0,
+            com     = new Vector();
+        for(let o of particles){
+            com.x  += o.position.x*o.mass;
+            com.y  += o.position.y*o.mass;
+            sum    += o.mass;
+        }
+        com.x      /= sum;
+        com.y      /= sum;
+        return com;
+    }
+
+    /**
+     *  Gravity calculations
+     */
+    gravity(){
+        for(let o of this.particles){
+            var g = new Vector();
+            if(this.particles.length > 1)
+                for(let i of this.particles){
+                    if(o == i) continue;
+                    let s   = new Vector(o.position.x-i.position.x,o.position.y-
+                                i.position.y,0),
+                        a   = i.mass/(s.magnitude()**2);
+                    g.x	   += s.cos()*a;
+                    g.y	   += s.sin()*a;
+                }
+            o.velocity.x   += g.x*(-this.G)*this.t;
+            o.velocity.y   += g.y*(-this.G)*this.t;
+        };
+        this.step.next().value();
+    }
+
+    /**
+     *  Move all particles forward according to their velocity
+     */
+    move(){
+        for(let o of this.particles){
+            o.position.x   += o.velocity.x*this.t;
+            o.position.y   += o.velocity.y*this.t;
+        }
+    }
+
+    /**
+     *  Increment time forward one tick
+     */
+    tick(){
+        if(!this.running) return !1;
+        this.step = (function*(s){
+            for(let i of s)
+                yield i;
+        })(this.steps);
+        this.step.next().value();
+    }
+}
+
+class Particle {}
+
+class Vector {}
+
+
+
 const phys	= {
     centerOfMass(){
         var sum		= 0,
